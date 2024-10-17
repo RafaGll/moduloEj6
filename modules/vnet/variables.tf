8c24b939f428ec329e04840f0e13b1a70877edda
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2024-10-17 20:22:34 +0200

    Update variables.tf

--- modules/vnet/variables.tf
+++ modules/vnet/variables.tf
@@ -1,14 +1,11 @@
 variable "resource_group_name" {
   type        = string
   description = "Nombre del resource group"
-  default = "rg-rgonzalez-dvfinlab"
 }
 
 variable "vnet_name" {
   type        = string
   description = "Nombre de la red virtual"
-  default = "vnetrafatfexercise05"
-
   validation {
     condition     = can(regex("^vnet[a-z]{3,}tfexercise[[:digit:]]{2,}$", var.vnet_name))
     error_message = "La cadena no cumple las carácterísticas requeridas"
@@ -18,23 +15,18 @@ variable "vnet_name" {
 variable "vnet_address_space" {
   type        = list(string)
   description = "Espacio de direcciones de la red virtual"
-  default = [ "10.0.0.0/16" ]
 }
 
 variable "location" {
   type = string
   #default     = "West Europe"
   description = "Ubicación donde se desplegará la red"
-  default = "West Europe"
-
 }
 
 # TAGS
 variable "owner_tag" {
   type        = string
   description = "Propietario de la red"
-  default = "rafagll"
-
   validation {
     condition     = var.owner_tag != "" && var.owner_tag != null
     error_message = "owner_tag no puede ser una cadena vacía o nula"
@@ -47,8 +39,6 @@ locals {
 variable "environment_tag" {
   type        = string
   description = "Entorno de la red"
-  default = "dev"
-
   validation {
     condition     = contains(local.entornos_aceptados, lower(var.environment_tag))
     error_message = "environment_tag debe ser 'dev', 'pro', 'tes' o 'pre'"
